fcfecc6f1d38fcb96f6a54ea03682b762e7af5c4
edsonlr <elribeirom10@gmail.com>
2023-05-19 16:06:17 -0300

    Product pageable

--- src/main/java/com/devsuperior/demo/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/demo/repositories/ProductRepository.java
@@ -2,6 +2,8 @@ package com.devsuperior.demo.repositories;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,4 +13,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	@Query(value = "SELECT obj FROM Product obj JOIN FETCH obj.categories")
 	List<Product> searchAll();
+	
+	@Query(value = "SELECT obj FROM Product obj JOIN FETCH obj.categories",
+			countQuery = "SELECT COUNT(obj) FROM Product obj JOIN obj.categories")
+	Page<Product> searchAll(Pageable pageable);
 }
